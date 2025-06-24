2691f24d03220dbf34c33f8322a1da901f839a2c
Cameron Beeche <cabeeche@gmail.com>
2025-06-24 16:47:40 -0400

    updates

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ from train_operations.percival import percival
 
 
 def single_inference_CT():
-    test_img_path = ''
+    test_img_path = '/cbica/projects/pmbb-vision/subjects/D894/4703/PMBBD8944703016/33925299/2.25.147678388709264164365783830345484277539/PMBBD8944703016_33925299_20120220075714_2_ABD_PELVIS_5_0_B30f/PMBBD8944703016_33925299_20120220075714_2_ABD_PELVIS_5_0_B30f_e1.nii.gz'
     print(test_img_path)
     img_weights = '/cbica/home/beechec/research/model_weights/foundation_percival/percival_checkpoint/weights/image_encoder_epoch_1_loss_1839.1098633.pth'
     in_channels = 1
